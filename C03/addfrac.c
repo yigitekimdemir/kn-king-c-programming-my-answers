f43d2a9acf306981b924952a2f459a88c5806041
Yiğit Ekim Demir <36767828+yigitekimdemir@users.noreply.github.com>
2018-03-06 19:20:29 +0300

    Add files via upload

---
+++ C03/addfrac.c
@@ -0,0 +1,22 @@
+/* Adds two fractions */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, resultNum, resultDenom;
+
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+
+    resultNum = num1 * denom2 + num2 * denom1;
+
+    resultDenom = denom1 * denom2;
+
+    printf("The sum is %d/%d\n", resultNum, resultDenom);
+
+    return 0;
+}
